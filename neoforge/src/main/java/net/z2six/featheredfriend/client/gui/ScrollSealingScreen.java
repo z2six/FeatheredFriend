f316fd6a1c66efb23aecc0734b793609b4da4943
Z2SIX <bathofredemption@duck.com>
2025-12-08 13:24:18 +0100

    increased date width to avoid "A.N." clipping

--- neoforge/src/main/java/net/z2six/featheredfriend/client/gui/ScrollSealingScreen.java
+++ neoforge/src/main/java/net/z2six/featheredfriend/client/gui/ScrollSealingScreen.java
@@ -191,7 +191,7 @@ public class ScrollSealingScreen extends AbstractContainerScreen<ScrollSealingMe
     // Date field config (relative to GUI origin)
     private static final int DATE_X = 30;
     private static final int DATE_Y = 18;
-    private static final int DATE_WIDTH = 125;
+    private static final int DATE_WIDTH = 150;
     private static final int DATE_HEIGHT = 14;
     private static final int DATE_MAX_CHARS = 64;
 
